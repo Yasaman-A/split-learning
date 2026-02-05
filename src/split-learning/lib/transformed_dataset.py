30ab8d7382bb6ab531b6ca0a07e09dbd7ef790d8
Samuel Trepac <strep688@mtroyal.ca>
2026-02-04 18:18:07 -0700

    added transformed_dataset.py

---
+++ src/split-learning/lib/transformed_dataset.py
@@ -0,0 +1,13 @@
+class TransformedDataset(torch.utils.data.Dataset):
+    def __init__(self, data, transform=None):
+        self.data = data
+        self.transform = transform
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        image, label = self.data[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
